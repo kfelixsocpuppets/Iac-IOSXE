f6eff8298225bf5e1f8b3889d056e9f8e6468842
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2025-12-01 13:37:05 -0600

    Create vlan.tf

---
+++ vlan.tf
@@ -0,0 +1,50 @@
+resource "iosxe_vlan" "DATA" {
+  id   = 100
+  name = "DATA"
+  shutdown = false
+}
+
+resource "iosxe_vlan" "VOICE" {
+  vlan_id = 120
+  name    = "VOICE"
+  shutdown = false
+}
+
+resource "iosxe_vlan" "STORAGE" {
+  vlan_id = 130
+  name    = "STORAGE"
+  shutdown = false
+}
+
+resource "iosxe_vlan" "CAMERAS" {
+  vlan_id = 140
+  name    = "CAMERAS"
+  shutdown = false
+}
+
+resource "iosxe_vlan" "DMZ01" {
+  vlan_id = 201
+  name    = "DMZ01"
+  shutdown = false
+}
+
+
+resource "iosxe_vlan" "DMZ02" {
+  vlan_id = 202
+  name    = "DMZ02"
+  shutdown = false
+}
+
+
+resource "iosxe_vlan" "DMZ03" {
+  vlan_id = 203
+  name    = "DMZ01"
+  shutdown = false
+}
+
+resource "iosxe_vlan" "DMZ04" {
+  vlan_id = 204
+  name    = "DMZ04"
+  shutdown = false
+}
+
